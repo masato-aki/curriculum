3755cb434b44d3505edd81cfd239408d6dd1ae57
masato-aki <120651121+masato-aki@users.noreply.github.com>
2023-02-01 00:16:36 +0900

    課題提出４章
    再提出します

---
+++ JAVAjob/4-1/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		private String firstName = "Aki"
+		private String lastName = "Masato"
+		
+    
+	private void printName(String firstName,String lastName) {
+			System.out.println(firstName + lastName);
+			printName();
+		}
+	}
+	
+	Pet pet = new Pet("java吉","hoge");
+	RobotPet robotpet = new RobotPet("R2D2","ルーク");
+	
+	
+	
+	
+	

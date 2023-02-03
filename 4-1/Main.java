17e67e790016f9a91f9ff69acc345ba7dfaff958
masato-aki <120651121+masato-aki@users.noreply.github.com>
2023-02-03 20:59:58 +0900

    再再々提出
    おねがいします

--- 4-1/Main.java
+++ 4-1/Main.java
@@ -1,20 +1,27 @@
 
 public class Main {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		private String firstName = "Aki"
-		private String lastName = "Masato"
+		
+		private String firstName=("aki");
+		private String lastName=("masato");
 		
     
 	private void printName(String firstName,String lastName) {
 			System.out.println(firstName + lastName);
-			printName();
-		}
-	}
+		
+	
+
+	
 	
 	Pet pet = new Pet("java吉","hoge");
 	RobotPet robotpet = new RobotPet("R2D2","ルーク");
+}
+}
+
 	
 	
 	

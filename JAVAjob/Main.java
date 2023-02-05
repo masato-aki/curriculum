29c02d13167f84599f463ce1b6a9f76b951d589b
masato-aki <120651121+masato-aki@users.noreply.github.com>
2023-02-05 21:25:13 +0900

    こちらです！再々再再

---
+++ JAVAjob/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		private String firstName=("aki");
+		private String lastName=("masato");
+		
+	Pet pet = new Pet("java吉","hoge");
+	RobotPet robotpet = new RobotPet("R2D2","ルーク");
+}
+}
+
+private void printName(String firstName,String lastName) 
+	System.out.println(firstName + lastName)
+
+
+	
+	
+	
+	
+	

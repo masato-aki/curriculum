e516fcdfd75eccc2eeb1637c7cdae21a59878251
masato-aki <120651121+masato-aki@users.noreply.github.com>
2023-05-30 21:48:25 +0900

    Delete 10-1 directory

--- 10-1/Numbers.js
+++
@@ -1,8 +0,0 @@
-let numbers = [2, 5, 12, 13, 15, 18, 22];
-//ここに答えを実装してください。↓↓↓
-for (let i = 0; i < numbers.length; i++) {
-    let num = numbers[i]
-    function isEven() {
-    console.log(num + 'は偶数です');
-}
-}

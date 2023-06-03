55faf5981283d2a533e862c5378774ddcc8c249e
masato-aki <120651121+masato-aki@users.noreply.github.com>
2023-06-04 01:49:47 +0900

    Add files via upload

---
+++ 10-1/main.js
@@ -0,0 +1,41 @@
+let numbers = [2, 5, 12, 13, 15, 18, 22];
+//ここに答えを実装してください。↓↓↓
+for (let i = 0; i < numbers.length; i++) {
+    let num = numbers[i]
+    if(numbers[i]%2===0){
+        console.log(numbers[i]);
+    function isEven() {
+        console.log(num + 'は偶数です');
+        
+}
+}
+}
+
+isEven();
+
+
+class Car {
+    
+    constructor(gas,num) {
+        this.gas = gas;
+        this.num = num; 
+    }
+
+getNumGas(){
+    console.log(`ガソリンは${this.gas}です。ナンバーは${this.num}です`);
+    }
+}
+let gasNum = new Car("油",0);
+
+
+gasNum.getNumGas();
+
+
+
+
+
+
+
+
+
+
